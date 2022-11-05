931f8d043b626ff889d3d97a7e85a7b926fd2cba
jinwon Hyun <94epicenter@gmail.com>
2022-11-05 22:50:29 +0900

    mybatis

--- src/test/java/com/apple/fox/FoxApplicationTests.java
+++ src/test/java/com/apple/fox/FoxApplicationTests.java
@@ -1,13 +1,28 @@
 package com.apple.fox;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@RunWith(SpringRunner.class) JUnit5부터는 아래의 것을 사용하게 되었다.
+@ExtendWith(SpringExtension.class)
 
 @SpringBootTest
 class FoxApplicationTests {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testSqlSession() throws Exception {
+		System.out.println(sqlSession.toString());
 	}
 
 }
